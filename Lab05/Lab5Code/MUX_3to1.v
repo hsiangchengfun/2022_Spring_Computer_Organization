2d45952944df04edd21e4b24b65a6f2d4803c4ac
hsiangchengfun <610467hc@gmail.com>
2022-05-21 14:22:20 +0800

    shiftleft mux files edited

--- Lab05/Lab5Code/MUX_3to1.v
+++ Lab05/Lab5Code/MUX_3to1.v
@@ -1,12 +1,23 @@
 `timescale 1ns/1ps
 
+// module MUX_3to1(
+//     input       [32-1:0] data0_i,
+//     input       [32-1:0] data1_i,
+//     input       [32-1:0] data2_i,
+//     input       [ 2-1:0] select_i,
+//     output  reg [32-1:0] data_o
+// );
 module MUX_3to1(
     input       [32-1:0] data0_i,
     input       [32-1:0] data1_i,
     input       [32-1:0] data2_i,
     input       [ 2-1:0] select_i,
-    output  reg [32-1:0] data_o
+    output      [32-1:0] data_o
 );
 /* Write your code HERE */
+assign data_o = (select_i == 2'b00)? data0_i : (select_i == 2'b01)? data1_i : data2_i ;
+
+
+
 endmodule
 
--- Lab05/Lab5Code/Shift_Left_1.v
+++ Lab05/Lab5Code/Shift_Left_1.v
@@ -1,8 +1,15 @@
 `timescale 1ns/1ps
 
+// module Shift_Left_1(
+//     input  		[32-1:0] data_i,
+//     output reg  [32-1:0] data_o
+//     );
 module Shift_Left_1(
     input  		[32-1:0] data_i,
-    output reg  [32-1:0] data_o
+    output      [32-1:0] data_o
     );
 /* Write your code HERE */
+
+assign data_o = { data_i[31-1:0],1'b0 };
+
 endmodule
